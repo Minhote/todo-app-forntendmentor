3862fc3bd58c9ee8c2086710fb8a9e962b7c63c0
Erick <erick_icaza_94@hotmail.com>
2022-10-04 15:09:26 -0500

    Styles Done without responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('hola');
+// Variables
+let ul = document.querySelector(".app__box__list");
+let input = document.querySelector(".app__bar__input");
+
+// Fill the list
+if (ul.innerText == "") ul.innerText = "Empty List";
+
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode == 13 && e.target.value != "") {
+    console.log("Crea");
+  }
+});
