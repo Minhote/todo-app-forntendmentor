3442eaa9b9e3f57a4d9d7fd12a83e22afd80ea74
Erick <erick_icaza_94@hotmail.com>
2022-10-05 15:07:52 -0500

    Functionality to create TODOS ready

--- src/main.js
+++ src/main.js
@@ -3,10 +3,32 @@ let ul = document.querySelector(".app__box__list");
 let input = document.querySelector(".app__bar__input");
 
 // Fill the list
-if (ul.innerText == "") ul.innerText = "Empty List";
+function emtptyOrNot() {
+  if (ul.innerText == "") {
+    ul.innerText = "Empty List";
+    ul.style.alignItems = "center";
+  } else if (ul.innerText == "Empty List") {
+    ul.innerText = "";
+    ul.style.alignItems = "start";
+  }
+}
+
+emtptyOrNot();
 
 input.addEventListener("keyup", (e) => {
   if (e.keyCode == 13 && e.target.value != "") {
-    console.log("Crea");
+    emtptyOrNot();
+    ul.insertAdjacentHTML(
+      "beforeend",
+      `
+    <li class="app__box__list__item">
+      <div class="app__box__list__item__wrap">
+        <span class="app__box__list__item__wrap__icon"></span>
+      </div> 
+      <p class="app__box__list__item__task">${e.target.value}</p>
+    </li>
+  `
+    );
+    e.target.value = "";
   }
 });
